6f2569c8f29e1662f63d412b4ef3f904c8a93b52
Rafael Jung <jungrafael@jungsoft.com.br>
2020-08-01 16:10:18 -0300

    Create CRON_TAIL file and folders automatically

--- scripts/docker-cmd.sh
+++ scripts/docker-cmd.sh
@@ -4,6 +4,7 @@ set -e
 if [ ! -z "$CRON_TAIL" ] 
 then
 	# crond running in background and log file reading every second by tail to STDOUT
+	mkdir -p "`dirname $CRON_TAIL`" && touch "$CRON_TAIL"
 	crond -s /var/spool/cron/crontabs -b -L $CRON_TAIL "$@" && tail -f $CRON_TAIL
 else
 	# crond running in foreground. log files can be retrive from /var/log/cron mount point
