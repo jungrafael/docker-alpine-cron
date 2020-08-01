c2161d0652e2e81f9858b07044923c8698c202cb
Rafael Jung <jungrafael@jungsoft.com.br>
2020-08-01 15:17:51 -0300

    Allow setting the log variable

--- scripts/docker-cmd.sh
+++ scripts/docker-cmd.sh
@@ -4,7 +4,7 @@ set -e
 if [ ! -z "$CRON_TAIL" ] 
 then
 	# crond running in background and log file reading every second by tail to STDOUT
-	crond -s /var/spool/cron/crontabs -b -L /var/log/cron/cron.log "$@" && tail -f /var/log/cron/cron.log
+	crond -s /var/spool/cron/crontabs -b -L $CRON_TAIL "$@" && tail -f $CRON_TAIL
 else
 	# crond running in foreground. log files can be retrive from /var/log/cron mount point
 	crond -s /var/spool/cron/crontabs -f -L /var/log/cron/cron.log "$@"
